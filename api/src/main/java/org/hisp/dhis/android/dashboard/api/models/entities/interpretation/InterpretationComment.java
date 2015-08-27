3f91b5e2f4c572587f12bf6791e8d53c07269dd0
Araz Abishov <araz.abishov.gsoc@gmail.com>
2015-08-27 10:16:24 +0200

    Refactoring interpretation models.

---
+++ api/src/main/java/org/hisp/dhis/android/dashboard/api/models/entities/interpretation/InterpretationComment.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright (c) 2015, dhis2
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ *  Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.hisp.dhis.android.dashboard.api.models.entities.interpretation;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import org.hisp.dhis.android.dashboard.api.models.BaseIdentifiableObject;
+import org.hisp.dhis.android.dashboard.api.models.Interpretation;
+import org.hisp.dhis.android.dashboard.api.models.User;
+import org.hisp.dhis.android.dashboard.api.models.entities.common.meta.State;
+
+/**
+ * @author Araz Abishov <araz.abishov.gsoc@gmail.com>.
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public final class InterpretationComment extends BaseIdentifiableObject {
+
+    @JsonProperty("text")
+    String text;
+
+    @JsonProperty("user")
+    User user;
+
+    org.hisp.dhis.android.dashboard.api.models.Interpretation interpretation;
+
+    State state;
+
+    public InterpretationComment() {
+        state = State.SYNCED;
+    }
+
+    /**
+     * Performs soft delete of model. If State of object was SYNCED, it will be set to TO_DELETE.
+     * If the model is persisted only in the local database, it will be removed immediately.
+     */
+    public final void deleteComment() {
+        if (State.TO_POST.equals(getState())) {
+            super.delete();
+        } else {
+            setState(State.TO_DELETE);
+            super.save();
+        }
+    }
+
+    /**
+     * Method modifies the original comment text and sets TO_UPDATE as state,
+     * if the object was received from server. If the model was persisted only locally,
+     * the State will be the TO_POST.
+     *
+     * @param newText Edited text of comment.
+     */
+    public final void updateComment(String newText) {
+        setText(newText);
+
+        if (state != State.TO_DELETE && state != State.TO_POST) {
+            state = State.TO_UPDATE;
+        }
+
+        super.save();
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public org.hisp.dhis.android.dashboard.api.models.Interpretation getInterpretation() {
+        return interpretation;
+    }
+
+    public void setInterpretation(Interpretation interpretation) {
+        this.interpretation = interpretation;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+}
