a64fd13ff14531e63c9241ff88f8db014d029013
Araz Abishov <araz.abishov.gsoc@gmail.com>
2015-08-27 09:16:37 +0200

    Implementing business logic through services.

---
+++ api/src/main/java/org/hisp/dhis/android/dashboard/api/services/common/IService.java
@@ -0,0 +1,7 @@
+package org.hisp.dhis.android.dashboard.api.services.common;
+
+/**
+ * Created by arazabishov on 8/27/15.
+ */
+public interface IService {
+}
---
+++ api/src/main/java/org/hisp/dhis/android/dashboard/api/services/dashboard/IDashboardService.java
@@ -0,0 +1,16 @@
+package org.hisp.dhis.android.dashboard.api.services.dashboard;
+
+import org.hisp.dhis.android.dashboard.api.models.entities.dashboard.Dashboard;
+import org.hisp.dhis.android.dashboard.api.models.entities.dashboard.DashboardItemContent;
+import org.hisp.dhis.android.dashboard.api.services.common.IService;
+
+/**
+ * Created by arazabishov on 8/27/15.
+ */
+public interface IDashboardService extends IService {
+    Dashboard createDashboard(String name);
+
+    void updateDashboardName(Dashboard dashboard, String name);
+
+    boolean addDashboardContent(Dashboard dashboard, DashboardItemContent content);
+}
