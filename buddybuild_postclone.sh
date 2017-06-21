002e31c2bac1b88200ce657f1e9a3056020bb700
Ignacio Foche <nacho.foche@gmail.com>
2017-06-21 11:31:31 +0200

    Feature codecov coverage integration (#17)
    * Add CodeCov integrated with Jacoco and Buddybuild
    * fix lint errors and gradlew problems
    * add traces to script
    * change inflater call to pass lint check
    * removing lint failures only for api module
    * Introduce abortOnError false

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -1,7 +1,10 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # Definitions
 gitPath=$(git rev-parse --show-toplevel)
 
 # Generate last commit
 sh ${gitPath}/generate_last_commit.sh
+
+echo "Generate Test Coverage Report:"
+./gradlew build jacocoTestReport assembleAndroidTest
